0902061692a9b530250a9b84f285a33d2215ee71
Edgar Cole <edgarcolejr@gmail.com>
2021-02-09 21:50:19 -0800

    Checkpoint Charlie

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,9 @@
 
 X
 (src/EDU/UCSD/extension/Lesson2RegEx.java,3\2\32d4ffa74bb31a4ecc545b162be908ad3fef33a4
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+G
+Lesson-2-Assignment.iml,0\f\0f4922c1b158bf923902d1073a6b299a5cafe907
+E
+src/neighbor-dump.txt,3\d\3db2dfaa9dd52ead7bc2f2134685947be0f3c142
--- src/EDU/UCSD/extension/Lesson2RegEx.java
+++ src/EDU/UCSD/extension/Lesson2RegEx.java
@@ -1,5 +1,11 @@
 package EDU.UCSD.extension;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Lesson2RegEx {
 
     public void syntaxSummary() {
@@ -13,14 +19,48 @@ public class Lesson2RegEx {
 
     public static void main(String[] arguments) {
         Lesson2RegEx lesson2RegEx = new Lesson2RegEx();
-        String argument;
+        String fileName = null;
+        String data = null;
         try {
-            argument = arguments[0];
+            fileName = arguments[0];
         } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
             System.err.println("Oops, an argument is required!");
             lesson2RegEx.syntaxSummary();
             System.exit(1);
         }
 
+        try {
+            File file = new File(fileName);
+            try (Scanner scanner = new Scanner(file)) {
+                scanner.useDelimiter("\\Z");
+                data = scanner.next();
+
+            }
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+            System.exit(1);
+        }
+
+        System.out.printf("%n%s%n%s%n%s%n",
+                "Processed the following input file:",
+                fileName,
+                "Results are as follows:");
+
+        list_Of_PANIDs(data);
+        list_Of_MAC_Addresses(data);
+        list_Of_RF_RSSI_M_Values(data);
+        
+    }
+
+    private static void list_Of_RF_RSSI_M_Values(String data) {
+        System.out.println(data);
+    }
+
+    private static void list_Of_MAC_Addresses(String data) {
+        System.out.println(data);
+    }
+
+    private static void list_Of_PANIDs(String data) {
+        System.out.println(data);
     }
 }
